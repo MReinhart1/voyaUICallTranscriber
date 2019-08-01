266d013225bce8d89a98c9ff541015ba43d41d11
Michael Reinhart <michael@Michaels-MacBook-Pro.local>
2019-08-01 19:49:08 -0400

    a little style added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # voyaUICallTranscriber
+
+
+This is a flask app that integrates with aws dynamobd and lambda to give voya
+employees the options to transcribe any calls that they may have saved from
+customers calling in to the call center
--- application.py
+++ application.py
@@ -10,7 +10,7 @@ import transcribe
 
 # App config.
 DEBUG = True
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/templates')
 app.config.from_object(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
